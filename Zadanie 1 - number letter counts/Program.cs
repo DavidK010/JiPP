9ab97382ff7147b4e1edd38c7817e061938cf609
DavidK010 <93396076+DavidK010@users.noreply.github.com>
2023-06-09 23:04:36 +0200

    Add files via upload

---
+++ Zadanie 1 - number letter counts/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int liczba = 0;
+Console.Write("Podaj swoją liczbę: "); 
+
+
+try //blok kodu do sprawdzenia
+{
+    liczba = Convert.ToInt32(Console.ReadLine());//próba przekonwertowania inputu usera na liczbe
+}
+catch //blok kodu w przypadku wystąpienia błędu w try
+{
+    Console.WriteLine("Nieprawidłowe dane wejściowe");
+    return;
+}
+
+if (liczba == 0)
+{
+    Console.WriteLine("Zero!");
+    return; 
+}
+if(liczba == 1000)
+{
+    Console.WriteLine("Tysiąc!");
+    return;
+}
+else if (liczba < 0)
+{
+    Console.WriteLine("Minus " + NaPolski(Math.Abs(liczba)));
+}    
+
+
+Console.WriteLine(NaPolski(liczba)); //wywołanie metody NaPolski na ekran
+
+
+static string NaPolski(int liczba)
+{
+
+    //Tablica elementów string do tłumaczenia zmiennej liczba na jezyk polski
+    string[] jednostki = { "", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem", "dziewięć" };
+    string[] nastki = { "dziesięć", "jedenaście", "dwanaście", "trzynaście", "czternaście", "piętnaście", "szesnaście", "siedemnaście", "osiemnaście", "dziewiętnaście" };
+    string[] dziesiatki = { "", "dziesięć", "dwadzieścia", "trzydzieści", "czterdzieści", "pięćdziesiąt", "sześćdziesiąt", "siedemdziesiąt", "osiemdziesiąt", "dziewięćdziesiąt" };
+    string[] setki = { "", "sto", "dwieście", "trzysta", "czterysta", "pięćset", "sześćset", "siedemset", "osiemset", "dziewięćset" };
+
+    string wynik = "";//zmienna do przechowywania wyniku tlumaczenia
+    if ((liczba / 100) > 0)
+    {
+        wynik += setki[liczba / 100] + " ";
+        liczba %= 100;
+    }
+
+    if (liczba >= 10 && liczba <= 19)
+    {
+        wynik += nastki[liczba - 10] + " ";
+        liczba = 0;
+    }
+    else if (liczba >= 20)
+    {
+        wynik += dziesiatki[liczba / 10] + " ";
+        liczba %= 10;
+    }
+
+    if (liczba > 0)
+    {
+        wynik += jednostki[liczba] + " ";
+    }
+
+
+    return wynik;
+}
---
+++ Zadanie 3 - system zarządzania biblioteką/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Console.WriteLine("zzzz");
+
+Ksiazka ksiazka1 = new Ksiazka("Ksiazka1", "Autor1", 1763, "Wydawca1", "ISBN1", "Gatunek1", 3);
+Ksiazka ksiazka2 = new Ksiazka("Ksiazka2", "Autor2", 1963, "Wydawca2", "ISBN2", "Gatunek2", 14);
+
+Biblioteka biblioteka = new Biblioteka();
+biblioteka.dodajKsiazke(ksiazka1);
+biblioteka.dodajKsiazke(ksiazka2);
+
+
+List<Ksiazka> szukaj = biblioteka.poTytule("Autor1");
+foreach (Ksiazka ksiazka in szukaj)
+{
+    Console.WriteLine("Tytul: " + ksiazka.Tytul);
+    Console.WriteLine("Autor: " + ksiazka.Autor);
+}
+
+
+
+
+//Klasa reprezentująca książkę
+public class Ksiazka
+{
+    public string Tytul { get; set; }
+    public string Autor { get; set; }
+    public int RokWydania { get; set; }
+    public string Wydawca { get; set; }
+    public string ISBN { get; set; }
+    public string Gatunek { get; set; }
+    public int DostepneEgz { get; set; }
+
+
+    public Ksiazka(string tytul, string autor, int rokwydania, string wydawca, string isbn, string gatunek, int dostepneegz)
+    //Konstruktor klasy Ksiazka, który inicjalizuje obiekt Ksiazka
+    {
+        Tytul = tytul;
+        Autor = autor;
+        RokWydania = rokwydania;
+        Wydawca = wydawca;
+        ISBN = isbn;
+        Gatunek = gatunek;
+        DostepneEgz = dostepneegz;
+    }
+
+}
+
+//Klasa reprezentujaca biblioteke
+public class Biblioteka
+{
+    private List<Ksiazka> ksiazkiLista;//Lista przechowujaca ksiazki
+
+    public Biblioteka()
+    {
+        ksiazkiLista = new List<Ksiazka>();
+    }
+
+    public void dodajKsiazke(Ksiazka ksiazka)
+    {
+        ksiazkiLista.Add(ksiazka);
+    }
+
+   
+
+    public List<Ksiazka> poTytule(string tytul)
+    {
+        return ksiazkiLista.FindAll(b => b.Tytul.Contains(tytul));
+        //b przyjmuje postac obiektu typu Ksiazka, b.tytul.contains(tytul) sprawdza, czy tytul znajduje sie w tytule ksiazki b
+    }
+
+}
+
